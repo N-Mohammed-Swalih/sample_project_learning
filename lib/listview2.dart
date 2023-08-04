0f51f2e1ceb373af83d5a7780c56da6d5cff6d29
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-04 16:09:54 +0530

    commit

--- lib/listview2.dart
+++ lib/listview2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -34,7 +35,13 @@ class ListView2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Sports World"),
+          title: Center(
+            child: Text("Sports World Valanchery",
+            style: GoogleFonts.roboto(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),),
+          ),
           backgroundColor: Colors.red,
         ),
         body: ListView(
