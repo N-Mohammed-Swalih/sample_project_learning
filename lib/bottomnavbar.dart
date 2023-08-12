50d2d5fa48f66f47a1a14292e13ad7d886d76d8e
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-12 11:20:32 +0530

    commit

--- lib/bottomnavbar.dart
+++ lib/bottomnavbar.dart
@@ -4,10 +4,11 @@ import 'Expanded.dart';
 import 'grid_view.dart';
 import 'grid_view_count.dart';
 import 'listview.dart';
-void main(){
+
+void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home : BottomNavBar(),
+    home: BottomNavBar(),
   ));
 }
 
@@ -19,54 +20,47 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-
-int index  =0;
-dynamic screens = [
-  ListView1(),
-const GridViewCount(),
-ExpandedWidget(),
-GridView1(),
-];
+  int index = 0;
+  dynamic screens = [
+    ListView1(),
+    const GridViewCount(),
+    ExpandedWidget(),
+    GridView1(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Bottom Nav Bar')
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-      //  backgroundColor: Colors.yellow,
-        selectedItemColor: Colors.red,
-        type: BottomNavigationBarType.shifting,
-        currentIndex: index,
-        onTap: (tappedindex){
-           setState(() {
-          index = tappedindex;
-        });},
-        items: 
-      [
-        BottomNavigationBarItem(
-          backgroundColor: Colors.black,
-          icon: Icon(Icons.home),
-          label:'Home'
-          ) ,
-           BottomNavigationBarItem(
-              backgroundColor: Colors.black,
-          icon: Icon(Icons.search),
-          label:'Search'
-          ) ,
-           BottomNavigationBarItem(
-              backgroundColor: Colors.black,
-          icon: Icon(Icons.video_library_outlined),
-          label:'Reels'
-          ) ,
-           BottomNavigationBarItem(
-              backgroundColor: Colors.black,
-          icon: Icon(Icons.person),
-          label:'Profile'
-          ) ,
-      ]),
-      body:Center(
-        child: screens[index],
-    ));
+        appBar: AppBar(title: Text('Bottom Nav Bar')),
+        bottomNavigationBar: BottomNavigationBar(
+            //  backgroundColor: Colors.yellow,
+            selectedItemColor: Colors.red,
+            type: BottomNavigationBarType.shifting,
+            currentIndex: index,
+            onTap: (tappedindex) {
+              setState(() {
+                index = tappedindex;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.black,
+                  icon: Icon(Icons.home),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.black,
+                  icon: Icon(Icons.search),
+                  label: 'Search'),
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.black,
+                  icon: Icon(Icons.video_library_outlined),
+                  label: 'Reels'),
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.black,
+                  icon: Icon(Icons.person),
+                  label: 'Profile'),
+            ]),
+        body: Center(
+          child: screens[index],
+        ));
   }
 }
