b665a14bf0c5dc5e80ee317daf9a83d87d55f246
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-23 16:51:43 +0530

    commit

--- lib/refactoring_3.dart
+++ lib/refactoring_3.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MaterialApp(home: Refractoring3(),));
+
+void main() {
+  runApp(MaterialApp(
+    home: Refractoring3(),
+  ));
 }
+
 class Refractoring3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,16 +14,20 @@ class Refractoring3 extends StatelessWidget {
         title: const Text("Refactoring 3"),
       ),
       body: GridView.builder(
-          gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2),
-          itemBuilder: (context,index){
+          itemBuilder: (context, index) {
             return MyRefractoringWidget(
-              rimage: Image.asset("assets/images/mango.png"),);
+              rimage: Image.network(
+                "https://images.unsplash.com/photo-1631143070457-c1aecc92abbc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+                fit: BoxFit.fill,
+              ),
+            );
           }),
     );
   }
 }
+
 class MyRefractoringWidget extends StatelessWidget {
   final Color? bgcolor;
   final Image? rimage;
@@ -27,17 +35,16 @@ class MyRefractoringWidget extends StatelessWidget {
   Widget? label;
   Widget? ricon;
 
-  MyRefractoringWidget({
-    this.bgcolor,
-    required this.rimage,
-    this.label,
-    this.onClick,
-    this.ricon
-});
+  MyRefractoringWidget(
+      {this.bgcolor,
+      required this.rimage,
+      this.label,
+      this.onClick,
+      this.ricon});
   @override
   Widget build(BuildContext context) {
     return Card(
-     color:  bgcolor,
+      color: bgcolor,
       child: ListTile(
         title: label,
         leading: rimage,
@@ -46,5 +53,4 @@ class MyRefractoringWidget extends StatelessWidget {
       ),
     );
   }
-
 }
