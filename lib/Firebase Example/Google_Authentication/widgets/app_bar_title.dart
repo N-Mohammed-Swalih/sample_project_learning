853b3f988a04d805006a9f25e1221c6c7d92fec2
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-10-15 11:15:00 +0530

    commit

---
+++ lib/Firebase Example/Google_Authentication/widgets/app_bar_title.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppBarTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/firebase_logo.png',
+          height: 20,
+        ),
+        const SizedBox(width: 8),
+        const Text(
+          'FlutterFire',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        const Text(
+          ' Authentication',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ],
+    );
+  }
+}
